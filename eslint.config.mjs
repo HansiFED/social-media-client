3933cc6195992c31ec289188889ff2783528eff0
Haze <117860981+HansiFED@users.noreply.github.com>
2024-10-18 14:08:31 +0200

    Features:
    - Added a 'test' test to see that eslint and jest works together
    
    - Fixed config for jest and eslint to work together

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,34 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import pluginJest from 'eslint-plugin-jest';
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {},
+  },
+
   pluginJs.configs.recommended,
+
+  {
+    files: ['**/*.test.js'],
+    plugins: {
+      jest: pluginJest,
+    },
+    languageOptions: {
+      globals: globals.jest,
+    },
+    rules: {
+      'jest/prefer-expect-assertions': 'off',
+    },
+    settings: {
+      jest: {
+        version: 27,
+      },
+    },
+  },
 ];
